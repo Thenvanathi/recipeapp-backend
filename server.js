cd07d3be967165cecbd9f384ffaf50ca40133d5c
Thenvanathi <thenvanathi2003@gmail.com>
2025-04-08 18:43:34 +0530

    Fixed axios + search route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch'); // for older Node.js
 const cors = require('cors');
-
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
